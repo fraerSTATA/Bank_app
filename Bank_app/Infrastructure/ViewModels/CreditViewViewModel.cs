ca2977da693efdd8c94b5cf72c656e7e1ed1ae4e
fraerSTATA <b.ym2002@yandex.ru>
2022-03-21 00:54:18 +0300

    Making Credits

---
+++ Bank_app/Infrastructure/ViewModels/CreditViewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Bank_app.DAL.Entityes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_app.Infrastructure.ViewModels
+{
+    internal class CreditViewViewModel : BaseViewModel
+    {
+        private int percent;
+        private int time;
+        private string name;
+
+        public CreditViewViewModel(CreditView a)
+        {
+            percent = a.percent_credit;
+            time = a.credit_time;
+            name = a.CreditType.type_name;
+        }
+
+        public int Percent { get => percent; set => Set(ref percent, value); }
+        public int Time { get => time; set => Set(ref time, value); }
+        public string Name { get => name; set => Set(ref name, value); }
+    }
+}
--- Bank_app/Infrastructure/ViewModels/MakeCreditViewModel.cs
+++ Bank_app/Infrastructure/ViewModels/MakeCreditViewModel.cs
@@ -1,9 +1,13 @@
 ﻿using Bank_app.DAL.Entityes;
+using Bank_app.Infrastructure.Services;
+using Bank_app.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace Bank_app.Infrastructure.ViewModels
 {
@@ -15,18 +19,35 @@ namespace Bank_app.Infrastructure.ViewModels
         private int workBook;
         private int salary;
         private int credit_sum;
-        private CreditView creditView;
+        private CreditView creditView;     
+        private readonly MakingCredit b;
+        private readonly IRepository<CreditView> views;
+        private ObservableCollection<Bank_app.Windows.DataTemplates.CreditView> credits =new ObservableCollection<Windows.DataTemplates.CreditView>();
 
         public User User { get { return user; } set => Set(ref user, value); }
 
-        
+        private void UpdateCreditViews()
+        {
+            foreach (var view in views.Items)
+            {
+                Bank_app.Windows.DataTemplates.CreditView creditView = new Bank_app.Windows.DataTemplates.CreditView();
+                CreditViewViewModel model = new CreditViewViewModel(view);
+                creditView.DataContext = model;
+                credits.Add(creditView);               
+            }
+            int h = 0;
+        }
         public int WorkBook { get => workBook; set => Set(ref workBook, value); }
         public int Salary { get => salary; set => Set(ref salary, value); }
         public int Credit_sum { get => credit_sum; set => Set(ref credit_sum, value); }
+        public ObservableCollection<Windows.DataTemplates.CreditView> Credits { get => credits; set => Set(ref credits, value); }
 
-        public MakeCreditViewModel(UserInterfaceViewModel a)
+        public MakeCreditViewModel(UserInterfaceViewModel a, MakingCredit b, IRepository<CreditView> views)
         {
             user = a.User;
+            this.b = b;
+            this.views = views;
+            UpdateCreditViews();
         }
     }
 }
